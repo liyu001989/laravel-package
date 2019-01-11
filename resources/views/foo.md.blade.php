3aaa2614ecc43a176289733462d639fa3f17e941
liyu <liyu001989@gmail.com>
2019-01-11 08:10:33 +0000

    graham-campbell/markdown

---
+++ resources/views/foo.md.blade.php
@@ -0,0 +1,27 @@
+# test markdown
+
+:smiley:
+
+:blush:
+
+**加粗**
+
+*斜体*
+
+_斜体_
+
+<del>删除线</del>
+
+<strong>加粗</strong>
+
+> > > > > > > > > > 测试缩进
+
+<script type="text/javascript">
+    // alert(123)
+</script>
+
+不安全的链接：[file](file://foo/bar.png)
+
+安全的链接： [google.com](http://google.com)
+
+![file](https://iocaffcdn.phphub.org/uploads/images/201808/06/1/0NBdJjC28V.png?imageView2/2/w/1240/h/0)
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('markdown', function() {
+    return view('foo');
+});
