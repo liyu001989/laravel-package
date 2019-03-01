dd9f2208040711dbfefde3f87acefd8bff23743c
liyu <liyu001989@gmail.com>
2019-03-01 14:21:27 +0800

    topics

---
+++ app/Topic.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
