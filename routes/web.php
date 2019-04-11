9c6ab54b18569a19bca7db7c339241cb7388ed95
liyu <liyu001989@gmail.com>
2019-04-11 09:08:29 +0000

    rodenastyle/stream-parser

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Tightenco\Collect\Support\Collection;
+use Rodenastyle\StreamParser\StreamParser;
+
+Route::get('xml', function() {
+    StreamParser::xml("https://www.w3schools.com/xml/plant_catalog.xml")->each(function(Collection $item){
+        dump($item);
+    });
+});
+
+Route::get('json', function() {
+    StreamParser::json("https://unpkg.com/province-city-china@2.0.5/dist/data.json")->each(function(Collection $item){
+        dump($item);
+    });
+});
+
+Route::get('csv', function() {
+    StreamParser::csv("https://unpkg.com/province-city-china@2.0.5/dist/data.csv")->each(function(Collection $item){
+        dump($item);
+    });
+});
