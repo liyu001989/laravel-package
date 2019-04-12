230b1c78b06906309d3c840af84c63f13bcff37b
liyu <liyu001989@gmail.com>
2019-04-12 09:51:37 +0000

    cmgmyr/messenger

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'messages', 'middleware' => 'auth'], function () {
+    Route::get('/', ['as' => 'messages', 'uses' => 'MessagesController@index']);
+    Route::get('create', ['as' => 'messages.create', 'uses' => 'MessagesController@create']);
+    Route::post('/', ['as' => 'messages.store', 'uses' => 'MessagesController@store']);
+    Route::get('{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@show']);
+    Route::put('{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
